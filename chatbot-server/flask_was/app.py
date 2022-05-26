ddf629d755301fa490405c5678b9347b0a0ed17b
이진욱 <jinwook@ijin-ug-ui-MacBookPro-2.local>
2022-05-26 16:02:15 +0900

    feat: 첫번째 인텐트 테스트 완료

--- chatbot-server/flask_was/app.py
+++ chatbot-server/flask_was/app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, make_response, json, request
+from orderDataType import OrderDataType
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "hi test"
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    return make_response(json.dumps(result()))
+
+
+def result():
+    req = request.get_json(force=True)
+    print("req : ", req)
+
+    queryText = req.get("queryResult").get("queryText")
+    print("queryText : ", queryText)
+
+    test_response = OrderDataType.response_type[queryText]
+    print(test_response)
+
+    respText = {
+        "fulfillmentMessages": [
+            {
+                "text":{
+                    "text":[
+                        "테스트 {}".format(test_response)
+                    ]
+                }
+            }
+        ]
+    }
+
+    return respText
 
 if __name__ == "__main__":
     app.run(port=8070)
